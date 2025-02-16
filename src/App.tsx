12ef7569bd4c9ee83f576f5605ae41eb80942d53
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-02-16 18:32:15 +0300

    Deleted NotFound page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import { Layout } from './layout/Layout'
 import { Hero } from './pages/hero/Hero'
 import { Home } from './pages/home/Home'
-import { NotFound } from './pages/not-found/NotFound'
 export const App = () => {
 	return (
 		<>
@@ -11,7 +10,6 @@ export const App = () => {
 					<Route path='/' element={<Layout />}>
 						<Route index path='/' element={<Home />}></Route>
 						<Route index path='/:name' element={<Hero />}></Route>
-						<Route path='*' element={<NotFound />}></Route>
 					</Route>
 				</Routes>
 			</BrowserRouter>
