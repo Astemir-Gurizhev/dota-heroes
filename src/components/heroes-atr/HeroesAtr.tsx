397b6eef4a79163b43cd2696436bb9a8d2a814f5
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-02-16 14:33:57 +0300

    Added size icon hero

--- src/components/heroes-atr/HeroesAtr.tsx
+++ src/components/heroes-atr/HeroesAtr.tsx
@@ -1,10 +1,10 @@
 import { HeroesAtrProps } from '../../types/types'
-
+import styles from "./HeroesAtr.module.css"
 export const HeroesAtr = ({ array, className }: HeroesAtrProps) => {
   return (
     <div className={className}>
       {array.map(item => (
-        <img key={item.id} className='icon' src={item.image} alt={item.name} />
+        <img key={item.id} className={`icon ${styles.heroIcon}`} src={item.image} alt={item.name} />
       ))}
     </div>
   );
