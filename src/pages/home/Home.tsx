c0f8be7ae8673eb7e0a4c637b2f9faf19e504018
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-02-16 13:44:30 +0300

    Added agi heroes

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,12 +3,24 @@ import styles from './Home.module.css'
 
 export const Home = () => {
 	const heroesStrength = heroes.filter(item => item.attr === 'strength')
+	const heroesAgi = heroes.filter(item => item.attr === 'agi')
 	return (
 		<div className={styles.heroes}>
-			{heroesStrength.map(item => (
-				<img className='icon' src={item.image} alt="" />
-				
-			))}
+			<div className={styles.heroesStrength}>
+				{heroesStrength.map(item => (
+					<img key={item.id} className='icon' src={item.image} alt='' />
+				))}
+			</div>
+			<div className={styles.heroesAgi}>
+				{heroesAgi.map(item => (
+					<img key={item.id} className='icon' src={item.image} alt='' />
+				))}
+			</div>
+			<div className={styles.heroesInt}>
+				{heroesStrength.map(item => (
+					<img key={item.id} className='icon' src={item.image} alt='' />
+				))}
+			</div>
 		</div>
 	)
 }
