501caf7479cd688a91fc266668be544479cc4641
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-02-16 14:15:46 +0300

    Added heroes int

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,20 +4,21 @@ import styles from './Home.module.css'
 export const Home = () => {
 	const heroesStrength = heroes.filter(item => item.attr === 'strength')
 	const heroesAgi = heroes.filter(item => item.attr === 'agi')
+	const heroesInt = heroes.filter(item => item.attr === 'int')
 	return (
 		<div className={styles.heroes}>
-			<div className={styles.heroesStrength}>
+			<div className={styles.heroesAtr}>
 				{heroesStrength.map(item => (
 					<img key={item.id} className='icon' src={item.image} alt='' />
 				))}
 			</div>
-			<div className={styles.heroesAgi}>
+			<div className={styles.heroesAtr}>
 				{heroesAgi.map(item => (
 					<img key={item.id} className='icon' src={item.image} alt='' />
 				))}
 			</div>
-			<div className={styles.heroesInt}>
-				{heroesStrength.map(item => (
+			<div className={styles.heroesAtr}>
+				{heroesInt.map(item => (
 					<img key={item.id} className='icon' src={item.image} alt='' />
 				))}
 			</div>
