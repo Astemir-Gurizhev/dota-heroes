7c613153f17b8c3618787f133ef3a01c8e49971d
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-02-16 13:12:03 +0300

    Added strength heroes

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,14 @@
+import heroes from '../../data/heroes.json'
+import styles from './Home.module.css'
+
 export const Home = () => {
-	return <div>Home</div>
+	const heroesStrength = heroes.filter(item => item.attr === 'strength')
+	return (
+		<div className={styles.heroes}>
+			{heroesStrength.map(item => (
+				<img className='icon' src={item.image} alt="" />
+				
+			))}
+		</div>
+	)
 }
-
-
